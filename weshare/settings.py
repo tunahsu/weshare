dc4618f27dba39479356aceb26d7e65f1c8239f3
Tuna Hsu <tunahsu45@gmail.com>
2020-06-19 04:11:40 +0800

    Changed method for environment management

--- weshare/settings.py
+++ weshare/settings.py
@@ -11,13 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from weshare.secret import *
 
-# user auth URL
-LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = '/account/login/'
-LOGOUT_REDIRECT_URL = '/account/login/'
-LOGOUT_URL = '/account/logout/'
 
 # auth backends
 AUTHENTICATION_BACKENDS =[
@@ -27,26 +21,9 @@ AUTHENTICATION_BACKENDS =[
     'social_core.backends.github.GithubOAuth2',
 ]
 
-# socail auth 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = DEV_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = DEV_SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
-SOCIAL_AUTH_GITHUB_KEY = DEV_SOCIAL_AUTH_GITHUB_KEY
-SOCIAL_AUTH_GITHUB_SECRET = DEV_SOCIAL_AUTH_GITHUB_SECRET
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = APP_SECRET_KEY
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['weshare.com']
 
 
 # Application definition
@@ -95,16 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'weshare.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -146,3 +113,11 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# user auth URL
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/account/login/'
+LOGOUT_REDIRECT_URL = '/account/login/'
+LOGOUT_URL = '/account/logout/'
+
+from weshare.env import *
